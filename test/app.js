0c33a462b0825bfc38b7c4ba1b58f7e68c2264d1
bendytree <josh@bendytree.com>
2013-08-29 11:47:57 -0500

    initial commit

---
+++ test/app.js
@@ -0,0 +1,28 @@
+
+var express = require('express')
+  , http = require('http')
+  , path = require('path');
+
+var app = express();
+
+GLOBAL.bundee = require('bundee')(app, { debug: false });
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'ejs');
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+app.get('/', function(req, res){
+  res.render('index');
+});
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
